73eb730558520edbbd7bdbbe3d7ccbaeab7f4182
Mrugank Akarte <mrugankakarte13@gmail.com>
2026-03-28 16:36:47 -0400

    Update post, rss

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,25 +1,27 @@
 import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
-// Import the constants we just created
 import { BLOG_TITLE, BLOG_DESCRIPTION } from '../consts';
+import sanitizeHtml from 'sanitize-html';
+import MarkdownIt from 'markdown-it';
+const parser = new MarkdownIt();
 
 export async function GET(context) {
   const posts = await getCollection('blog');
   return rss({
-    // Use the imported constants
     title: BLOG_TITLE,
     description: BLOG_DESCRIPTION,
-    // Pull in your site from your `astro.config.mjs`
     site: context.site,
-    // Array of `<item>`s in XML feed
     items: posts.map((post) => ({
       title: post.data.title,
       pubDate: post.data.pubDate,
       description: post.data.description,
       // Creates a valid link to the post
       link: `/blog/${post.slug}/`,
+      // For Rogue Scholar and other feed readers to index the full content
+      content: sanitizeHtml(parser.render(post.body), {
+        allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img'])
+      }),
     })),
-    // (Optional) Add a stylesheet to make the XML feed readable in the browser
     stylesheet: '/rss/styles.xsl',
   });
 }
